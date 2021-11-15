643b45217239cd13b6a5a7cfeb3632047c65a45d
Bruno da Silva Valenga <brunodasilvalenga@gmail.com>
2021-11-16 09:12:33 +1100

    lint file

--- examples/keda.tf
+++ examples/keda.tf
@@ -1,6 +1,6 @@
 module "eks-keda" {
-  source  = "DNXLabs/eks-keda/aws"
-  version = "0.0.3"
-  enabled  = true
-  helm_chart_version = "v2.4.0"
+  source             = "DNXLabs/eks-keda/aws"
+  version            = "0.0.3"
+  enabled            = true
+  helm_chart_version = "2.4.0"
 }
